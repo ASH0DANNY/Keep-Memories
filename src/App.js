b965be318b93707196fc11cd8a448de7c6038c57
ASH0DANNY <ash22714497@gmail.com>
2024-10-05 16:46:43 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Footer from "./Components/footer";
 import Navbar from "./Components/navbar";
 import Sidebar from "./Components/sidebar";
 import ToDoList from "./Components/ToDoList/todolist";
-import logo from "./logo.svg";
 
 function App() {
   return (
