805b049e2a1516d3fe02f40a656136b207fd93c7
ASH0DANNY <ash22714497@gmail.com>
2024-10-07 13:29:39 +0530

    Saved Password

---
+++ src/component/savePassword.jsx
@@ -0,0 +1,83 @@
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+
+const SavePassword = () => {
+  const [SavedPassword, setSavedPassword] = useState([]);
+  const [webLink, setWebLink] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [inputValue, setInputValue] = useState({
+    link: "",
+    username: "",
+    password: "",
+  });
+
+  const handleAddSavePassword = () => {
+    setInputValue({
+      link: webLink,
+      username: userName,
+      password: userPassword,
+    });
+
+    if (editIndex !== null) {
+      const updatedList = SavedPassword.map((item, index) =>
+        index === editIndex ? inputValue : item
+      );
+      setSavedPassword(updatedList);
+      setEditIndex(null);
+    } else {
+      setTodos([...SavedPassword, inputValue]);
+    }
+
+    setInputValue("");
+  };
+
+  const HandleUpdatePassword = (index) => {
+    setInputValue(SavedPassword[index]);
+    setEditIndex(index);
+  };
+
+  const HandleDeletePassword = (index) => {
+    const updatedList = SavedPassword.filter((_, i) => i !== index);
+    setSavedPassword(updatedList);
+  };
+
+  return (
+    <>
+      <h1>Save Password</h1>
+      <input
+        type="text"
+        value={webLink}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Web/App link or Name"
+      />
+      <input
+        type="text"
+        value={userName}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="User Name"
+      />
+      <input
+        type="text"
+        value={userPassword}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Password"
+      />
+      <Button onClick={handleAddSavePassword}>
+        {editIndex !== null ? "Add" : "Update"}
+      </Button>
+      <ul>
+        {SavedPassword.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => HandleUpdatePassword(index)}>Edit</button>
+            <button onClick={() => HandleDeletePassword(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default SavePassword;
