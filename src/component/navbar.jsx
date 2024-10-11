6c81644c0f5fb1d155d509f8d7e96317961166f2
John Doe <ash22714497@gmail.com>
2024-10-11 16:50:50 +0530

    .

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -3,28 +3,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const linkStyle = {
+    color: "white",
+    textDecoration: "none",
+  };
   return (
     <>
       <Box
         sx={{
           bgcolor: "#000",
           color: "#fff",
-          height: 40
+          height: 50,
         }}
       >
-        <Grid2 sm={6} xs={12}>LOGO/NAME</Grid2>
+        <Grid2 sm={6} xs={12}>
+          LOGO/NAME
+        </Grid2>
         <Grid2 container spacing={2}>
           <Grid2 item sm={4}>
-            <Link to="/">HOME</Link>
+            <Link to="/" style={linkStyle}>
+              HOME
+            </Link>
           </Grid2>
           <Grid2 item sm={4}>
-            <Link to="/todo">TODO</Link>
+            <Link to="/todo" style={linkStyle}>
+              TODO
+            </Link>
           </Grid2>
           <Grid2 item sm={4}>
-            <Link to="/savepassword">SavePass</Link>
+            <Link to="/savepassword" style={linkStyle}>
+              SavePass
+            </Link>
           </Grid2>
           <Grid2 item sm={4}>
-            <Link to="/add_birthday">Birthday</Link>
+            <Link to="/add_birthday" style={linkStyle}>
+              Birthday
+            </Link>
           </Grid2>
         </Grid2>
       </Box>
